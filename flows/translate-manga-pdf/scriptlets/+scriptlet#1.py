408a1867114e8ef870de4087aec9fd47d5b74f8a
shaun <beingswu@gmail.com>
2025-06-27 10:02:07 +0000

    update batch

---
+++ flows/translate-manga-pdf/scriptlets/+scriptlet#1.py
@@ -0,0 +1,15 @@
+from oocana import Context
+
+#region generated meta
+import typing
+class Inputs(typing.TypedDict):
+    input: typing.Any
+class Outputs(typing.TypedDict):
+    output: typing.Any
+#endregion
+
+def main(params: Inputs, context: Context) -> Outputs:
+
+    # your code
+
+    return { "output": "output_value" }
